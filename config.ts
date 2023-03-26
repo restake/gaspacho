9da728d5124feea14a56ec59f48096c70be59add
Markkus Millend <mm@liangroup.io>
2023-03-26 22:09:28 +0200

    Add dynamic price fetching via Coingecko API

--- config.ts
+++ config.ts
@@ -3,6 +3,8 @@ interface Config {
     VALIDATOR_ADDRESS: string;
     SUI_PRICE_USD: number;
     SERVER_COSTS_USD: number;
+    COINGECKO_API_URL: string;
+    COINGECKO_COIN_ID: string;
 }
 
 export default <Config> {
@@ -10,4 +12,6 @@ export default <Config> {
     VALIDATOR_ADDRESS: Deno.env.get("VALIDATOR_ADDRESS"),
     SUI_PRICE_USD: Number(Deno.env.get("SUI_PRICE_USD")),
     SERVER_COSTS_USD: Number(Deno.env.get("SERVER_COSTS_USD")),
+    COINGECKO_API_URL: Deno.env.get("COINGECKO_API_URL"),
+    COINGECKO_COIN_ID: Deno.env.get("COINGECKO_COIN_ID"),
 };
--- sui/sui_price.ts
+++ sui/sui_price.ts
@@ -1,3 +1,63 @@
+import Config from "../config.ts";
+
+interface CurrentPriceResponse {
+    [key: string]: {
+        usd: number;
+    };
+}
+
+interface PingResponse {
+    gecko_says: string;
+}
+
 export class SuiPrice {
-    // TODO: fetch SUI price from CoinGecko / CoinMarketCap.
+    static #apiUrl: string = Config.COINGECKO_API_URL;
+    static #coinId: string = Config.COINGECKO_COIN_ID;
+
+    // deno-lint-ignore no-explicit-any
+    private static async getJson(response: Response): Promise<any> {
+        return await response.json();
+    }
+
+    /**
+     * Get current token price from the Coingecko API.
+     *
+     * @returns Current price response.
+     */
+    private static async getCurrentPriceResponse(): Promise<CurrentPriceResponse> {
+        const query = new URLSearchParams({
+            ids: this.#coinId,
+            vs_currencies: "usd",
+        });
+        const response = await fetch(`${this.#apiUrl}/simple/price?${query.toString()}`, {
+            method: "GET",
+        });
+
+        return this.getJson(response);
+    }
+
+    /**
+     * Get ping response from the Coingecko API.
+     *
+     * @returns Ping response.
+     */
+    static async getPingResponse(): Promise<PingResponse> {
+        const response = await fetch(`${this.#apiUrl}/ping`, {
+            method: "GET",
+        });
+
+        return this.getJson(response);
+    }
+
+    /**
+     * Get the current price of Sui token in USD.
+     * Accessor for the internal #getCurrentPriceResponse method.
+     *
+     * @returns Token price in USD.
+     */
+    static async getCurrentPrice(): Promise<number> {
+        const response = await this.getCurrentPriceResponse();
+
+        return response[this.#coinId].usd;
+    }
 }
